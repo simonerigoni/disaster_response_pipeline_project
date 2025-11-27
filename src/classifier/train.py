e7e63519d608d1e63d6a21e8a81c6a8096c57985
simone <simone.rigoni01@gmail.com>
2025-11-27 10:19:41 +0100

    Fix formatting

--- src/classifier/train.py
+++ src/classifier/train.py
@@ -24,21 +24,21 @@ from src.config import DATABASE_FILENAME, TABLE_NAME, MODEL_PICKLE_FILENAME
 
 
 def get_df_from_database(database_filename=DATABASE_FILENAME):
-    '''
+    """
     Return dataframe from the database
 
     Args:
         database_filename (str): database filename. Default value DATABASE_FILENAME
 
     Returns:
-        df (pandas.DataFrame): dataframe containing the data 
-    '''
-    engine = create_engine('sqlite:///' + database_filename)
+        df (pandas.DataFrame): dataframe containing the data
+    """
+    engine = create_engine("sqlite:///" + database_filename)
     return pd.read_sql_table(TABLE_NAME, engine)
 
 
 def load_data(database_filename):
-    '''
+    """
     Load the data from the database
 
     Args:
@@ -48,16 +48,16 @@ def load_data(database_filename):
         X (pandas.Series): dataset
         Y (pandas.DataFrame): dataframe containing the categories
         category_names (list): list containing the categories name
-    '''
+    """
     df = get_df_from_database(database_filename)
-    X = df['message']
+    X = df["message"]
     Y = df.iloc[:, 4:]
     category_names = list(df.columns[4:])
     return X, Y, category_names
 
 
 def my_tokenizer(text):
-    '''
+    """
     Clean and tokenize the text in input
 
     Args:
@@ -65,11 +65,13 @@ def my_tokenizer(text):
 
     Returns:
         clean_tokens (list): tokens obtained from the input text
-    '''
-    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    """
+    url_regex = (
+        "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"  # type: ignore
+    )
     detected_urls = re.findall(url_regex, text)
     for url in detected_urls:
-        text = text.replace(url, 'urlplaceholder')
+        text = text.replace(url, "urlplaceholder")
 
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
@@ -83,7 +85,7 @@ def my_tokenizer(text):
 
 
 def build_model(grid_search_cv=False):
-    '''
+    """
     Build the model
 
     Args:
@@ -91,18 +93,26 @@ def build_model(grid_search_cv=False):
 
     Returns:
         pipeline (pipeline.Pipeline): model
-    '''
-    pipeline = Pipeline([('vect', CountVectorizer(tokenizer=my_tokenizer, token_pattern=None)),
-                         ('tfidf', TfidfTransformer()),
-                         ('clf', MultiOutputClassifier(RandomForestClassifier()))
-                         ])
+    """
+    pipeline = Pipeline(
+        [
+            ("vect", CountVectorizer(tokenizer=my_tokenizer, token_pattern="")),
+            ("tfidf", TfidfTransformer()),
+            ("clf", MultiOutputClassifier(RandomForestClassifier())),
+        ]
+    )
 
     # pipeline.get_params()
 
-    if grid_search_cv == True:
-        print('Searching for best parameters...')
-        parameters = {'vect__ngram_range': ((1, 1), (1, 2)), 'vect__max_df': (0.5, 0.75, 1.0), 'tfidf__use_idf': (True, False), 'clf__estimator__n_estimators': [50, 100, 200], 'clf__estimator__min_samples_split': [2, 3, 4]
-                      }
+    if grid_search_cv is True:
+        print("Searching for best parameters...")
+        parameters = {
+            "vect__ngram_range": ((1, 1), (1, 2)),
+            "vect__max_df": (0.5, 0.75, 1.0),
+            "tfidf__use_idf": (True, False),
+            "clf__estimator__n_estimators": [50, 100, 200],
+            "clf__estimator__min_samples_split": [2, 3, 4],
+        }
 
         pipeline = GridSearchCV(pipeline, param_grid=parameters)
 
@@ -110,7 +120,7 @@ def build_model(grid_search_cv=False):
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    '''
+    """
     Evaluate the model performances and print the results
 
     Args:
@@ -121,19 +131,25 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
     Returns:
         None
-    '''
+    """
     Y_pred = model.predict(X_test)
     # Calculate the accuracy for each of them.
     for i in range(len(category_names)):
-        print('Category: {} '.format(category_names[i]))
-        print(classification_report(
-            Y_test.iloc[:, i].values, Y_pred[:, i], zero_division=0))
-        print('Accuracy {}\n\n'.format(accuracy_score(
-            Y_test.iloc[:, i].values, Y_pred[:, i])))
+        print("Category: {} ".format(category_names[i]))
+        print(
+            classification_report(
+                Y_test.iloc[:, i].values, Y_pred[:, i], zero_division="0"
+            )
+        )
+        print(
+            "Accuracy {}\n\n".format(
+                accuracy_score(Y_test.iloc[:, i].values, Y_pred[:, i])
+            )
+        )
 
 
 def save_model(model, model_filename):
-    '''
+    """
     Save in a pickle file the model
 
     Args:
@@ -142,25 +158,25 @@ def save_model(model, model_filename):
 
     Returns:
         None
-    '''
-    pickle.dump(model, open(model_filename, 'wb'))
+    """
+    pickle.dump(model, open(model_filename, "wb"))
 
 
 def load_model(model_pickle_filename):
-    '''
+    """
     Return model from pickle file
 
     Args:
         model_pickle_filename (str): source pickle filename
 
     Returns:
-        model (pipeline.Pipeline): model readed from pickle file 
-    '''
-    return pickle.load(open(model_pickle_filename, 'rb'))
+        model (pipeline.Pipeline): model readed from pickle file
+    """
+    return pickle.load(open(model_pickle_filename, "rb"))
 
 
 def parse_input_arguments():
-    '''
+    """
     Parse the command line arguments
 
     Args:
@@ -170,22 +186,35 @@ def parse_input_arguments():
         database_filename (str): database filename. Default value DATABASE_FILENAME
         model_pickle_filename (str): pickle filename. Default value MODEL_PICKLE_FILENAME
         grid_search_cv (bool): If True perform grid search of the parameters
-    '''
+    """
     parser = argparse.ArgumentParser(
-        description="Disaster Response Pipeline Train Classifier")
-    parser.add_argument('--database_filename', type=str,
-                        default=DATABASE_FILENAME, help='Database filename of the cleaned data')
-    parser.add_argument('--model_pickle_filename', type=str,
-                        default=MODEL_PICKLE_FILENAME, help='Pickle filename to save the model')
-    parser.add_argument('--grid_search_cv', action="store_true",
-                        default=False, help='Perform grid search of the parameters')
+        description="Disaster Response Pipeline Train Classifier"
+    )
+    parser.add_argument(
+        "--database_filename",
+        type=str,
+        default=DATABASE_FILENAME,
+        help="Database filename of the cleaned data",
+    )
+    parser.add_argument(
+        "--model_pickle_filename",
+        type=str,
+        default=MODEL_PICKLE_FILENAME,
+        help="Pickle filename to save the model",
+    )
+    parser.add_argument(
+        "--grid_search_cv",
+        action="store_true",
+        default=False,
+        help="Perform grid search of the parameters",
+    )
     args = parser.parse_args()
     # print(args)
     return args.database_filename, args.model_pickle_filename, args.grid_search_cv
 
 
 def train(database_filename, model_pickle_filename, grid_search_cv=False):
-    '''
+    """
     Train the model and save it in a pickle file
 
     Args:
@@ -195,36 +224,36 @@ def train(database_filename, model_pickle_filename, grid_search_cv=False):
 
     Returns:
         None
-    '''
+    """
     # print(database_filename)
     # print(model_pickle_filename)
     # print(grid_search_cv)
     # print(os.getcwd())
 
-    print('Download nltk componets if needed...')
-    nltk.download(['punkt', 'punkt_tab', 'wordnet'])
+    print("Download nltk componets if needed...")
+    nltk.download(["punkt", "punkt_tab", "wordnet"])
 
-    print('Loading data...\n    Database: {}'.format(database_filename))
+    print("Loading data...\n    Database: {}".format(database_filename))
     X, Y, category_names = load_data(database_filename)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-    print('Building model...')
+    print("Building model...")
     model = build_model(grid_search_cv)
 
-    print('Training model...')
+    print("Training model...")
     model.fit(X_train, Y_train)
 
-    print('Evaluating model...')
+    print("Evaluating model...")
     evaluate_model(model, X_test, Y_test, category_names)
 
-    print('Saving model...\n    Model: {}'.format(model_pickle_filename))
+    print("Saving model...\n    Model: {}".format(model_pickle_filename))
     save_model(model, model_pickle_filename)
 
-    print('Trained model saved!')
+    print("Trained model saved!")
 
 
-if __name__ == '__main__':
-    print('Train the model and save it in a pickle file')
+if __name__ == "__main__":
+    print("Train the model and save it in a pickle file")
     database_filename, model_pickle_filename, grid_search_cv = parse_input_arguments()
     train(database_filename, model_pickle_filename, grid_search_cv)
 else:
